40a82cf547298511a7167a47e8ab6a5facb99778
Maxim Loginov <maxim.loginov@mail.ru>
2024-02-24 11:07:34 +0700

    first working version from 07.12.2023, no data

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# asmu-streamlit
 Скрипты streamlit для АГМУ
+- `machine_learning_images.py` для темы "Применение нейросетей для анализа медицинских данных" в курсе ["Основы цифровой медицины"](https://do.asmu.ru/course/view.php?id=5556166)
---
+++ machine_learning_images.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+
+import pathlib
+
+import numpy as np
+import skimage
+import torch
+import torchvision
+import matplotlib.pyplot as plt
+import torchxrayvision as xrv
+
+import streamlit as st
+
+
+#st.set_page_config(layout='wide')
+st.title('Сегментация анатомических структур')
+
+model = xrv.baseline_models.chestx_det.PSPNet()
+model.targets_ru = {
+    'Left Clavicle':          'Левая ключица',
+    'Right Clavicle':	      'Правая ключица',
+    'Left Scapula':	      'Левая лопатка',
+    'Right Scapula':          'Правая лопатка',
+    'Left Lung':              'Левое легкое',
+    'Right Lung':             'Правое легкое',
+    'Left Hilus Pulmonis':    '', #'Левый Hilus Pulmonis',
+    'Right Hilus Pulmonis':   '', #'Правый Hilus Pulmonis',
+    'Heart':                  'Сердце',
+    'Aorta':                  'Аорта',
+    'Facies Diaphragmatica':  '', #''Фация диафрагмальная',
+    'Mediastinum':            '', #''Средостение',
+    'Weasand':                '', #''Глотка',
+    'Spine':                  'Позвоночник',
+}
+
+files = pathlib.Path('data_padchest').glob('*.png')
+filename = st.selectbox('Выберите файл', [f for f in files])
+
+#img = skimage.io.imread("../tests/covid-19-pneumonia-58-prior.jpg")
+img = skimage.io.imread(filename)
+img = xrv.datasets.normalize(img, 255) # convert 8-bit image to [-1024, 1024] range
+#img = img.mean(2)[None, ...] # Make single color channel
+img = img[None, :, :] # works for PNG 8-bit sRGB 256c
+
+transform = torchvision.transforms.Compose([xrv.datasets.XRayCenterCrop(), xrv.datasets.XRayResizer(512)])
+img = transform(img)
+img = torch.from_numpy(img)
+
+with torch.no_grad():
+    pred = model(img)
+
+# plt.figure(figsize = (26,5))
+# plt.subplot(1, len(model.targets) + 1, 1)
+# plt.imshow(img[0], cmap='gray')
+# for i in range(len(model.targets)):
+#     plt.subplot(1, len(model.targets) + 1, i+2)
+#     plt.imshow(pred[0, i])
+#     plt.title(model.targets[i])
+#     plt.axis('off')
+# plt.tight_layout()
+
+pred = 1 / (1 + np.exp(-pred))  # sigmoid
+pred[pred < 0.5] = 0
+pred[pred > 0.5] = 1
+
+fig1, ax1 = plt.subplots(figsize = (4*4, 3*4))
+plt.subplot(4, 3, 1); plt.axis('off')
+plt.subplot(4, 3, 2)
+plt.imshow(img[0], cmap='gray')
+plt.subplot(4, 3, 3); plt.axis('off')
+fig_count = 4
+for i in range(len(model.targets)):
+    title_ru = model.targets_ru[model.targets[i]]
+    if not title_ru:
+        continue
+    plt.subplot(4, 3, fig_count)
+    plt.imshow(img[0], cmap='gray')
+    plt.contour(pred[0, i], [0.5], colors=['red'])
+    plt.title(title_ru)
+    plt.axis('off')
+    fig_count += 1
+plt.tight_layout()
+#plt.subplots_adjust(wspace=0, hspace=0)
+
+st.pyplot(fig1)
+
+st.title('Предсказание ')
+
+models = {
+#    'densenet121-res224-all': 'All',
+    'densenet121-res224-rsna': 'RSNA Pneumonia Challenge',
+    'densenet121-res224-nih': 'NIH chest X-ray8',
+    'densenet121-res224-pc': 'PadChest (University of Alicante)',
+    'densenet121-res224-chex': 'CheXpert (Stanford)',
+#    'densenet121-res224-mimic_nb': 'MIMIC-CXR (MIT)',
+#    'densenet121-res224-mimic_ch': 'MIMIC-CXR (MIT)',
+}
+modname = st.selectbox('Выберите модель', [m for m in models])
+model = xrv.models.get_model(modname)
+
+img = skimage.io.imread(filename)
+img = xrv.datasets.normalize(img, 255)
+img = img[None, :, :] # Add color channel
+
+transform = torchvision.transforms.Compose([xrv.datasets.XRayCenterCrop(), xrv.datasets.XRayResizer(224)])
+img = transform(img)
+img = torch.from_numpy(img).unsqueeze(0)
+
+fig2, ax2 = plt.subplots(figsize = (1, 3)) # figsize = (4*4, 3*4)
+plt.imshow(img[0][0], cmap='gray')
+plt.axis('off')
+st.pyplot(fig2)
