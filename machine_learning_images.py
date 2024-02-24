7131fec439b4df7b3a66cbdd8e668f67f3ccb453
Maxim Loginov <maxim.loginov@mail.ru>
2024-02-24 12:33:08 +0700

    working version for ASMU from 08.12.2023, no data

--- machine_learning_images.py
+++ machine_learning_images.py
@@ -13,7 +13,7 @@ import streamlit as st
 
 
 #st.set_page_config(layout='wide')
-st.title('Сегментация анатомических структур')
+st.title('Анатомическая сегментация')
 
 model = xrv.baseline_models.chestx_det.PSPNet()
 model.targets_ru = {
@@ -35,6 +35,7 @@ model.targets_ru = {
 
 files = pathlib.Path('data_padchest').glob('*.png')
 filename = st.selectbox('Выберите файл', [f for f in files])
+st.write(f'Файл: {filename}')
 
 #img = skimage.io.imread("../tests/covid-19-pneumonia-58-prior.jpg")
 img = skimage.io.imread(filename)
@@ -60,8 +61,8 @@ with torch.no_grad():
 # plt.tight_layout()
 
 pred = 1 / (1 + np.exp(-pred))  # sigmoid
-pred[pred < 0.5] = 0
-pred[pred > 0.5] = 1
+#pred[pred < 0.5] = 0
+#pred[pred > 0.5] = 1
 
 fig1, ax1 = plt.subplots(figsize = (4*4, 3*4))
 plt.subplot(4, 3, 1); plt.axis('off')
@@ -84,19 +85,20 @@ plt.tight_layout()
 
 st.pyplot(fig1)
 
-st.title('Предсказание ')
+st.title('Предсказание патологий')
 
 models = {
 #    'densenet121-res224-all': 'All',
-    'densenet121-res224-rsna': 'RSNA Pneumonia Challenge',
+#    'densenet121-res224-rsna': 'RSNA Pneumonia Challenge',
+    'densenet121-res224-chex': 'CheXpert (Stanford)',
     'densenet121-res224-nih': 'NIH chest X-ray8',
     'densenet121-res224-pc': 'PadChest (University of Alicante)',
-    'densenet121-res224-chex': 'CheXpert (Stanford)',
 #    'densenet121-res224-mimic_nb': 'MIMIC-CXR (MIT)',
 #    'densenet121-res224-mimic_ch': 'MIMIC-CXR (MIT)',
 }
 modname = st.selectbox('Выберите модель', [m for m in models])
 model = xrv.models.get_model(modname)
+pathologies = {p: model.pathologies.index(p) for p in model.pathologies if p}
 
 img = skimage.io.imread(filename)
 img = xrv.datasets.normalize(img, 255)
@@ -105,8 +107,21 @@ img = img[None, :, :] # Add color channel
 transform = torchvision.transforms.Compose([xrv.datasets.XRayCenterCrop(), xrv.datasets.XRayResizer(224)])
 img = transform(img)
 img = torch.from_numpy(img).unsqueeze(0)
+#img = torch.from_numpy(img)
+
+st.write(f'Модель {modname} ({models[modname]}) может предсказывать следующие патологии:')
+st.write(', '.join([p.replace("_", " ") for p in model.pathologies if p]))
+pathology = st.selectbox('Выберите патологию для визуализации',
+                         [p for p in pathologies])
+target = pathologies[pathology]
+img = img.requires_grad_()
+outputs = model(img)
+grads = torch.autograd.grad(outputs[:, target], img)[0][0][0]
+blurred = skimage.filters.gaussian(grads.detach().cpu().numpy()**2, sigma=(5, 5), truncate=3.5)
 
 fig2, ax2 = plt.subplots(figsize = (1, 3)) # figsize = (4*4, 3*4)
-plt.imshow(img[0][0], cmap='gray')
+plt.imshow(img[0][0].detach().cpu().numpy(), cmap='gray', aspect='auto')
+plt.imshow(blurred, alpha=0.3)
 plt.axis('off')
 st.pyplot(fig2)
+st.write(outputs[:, target])
